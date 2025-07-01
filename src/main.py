f745b44ee59e2e2e11fce7523ffa187d962aa47b
He Zhang <hershxy@sjtu.edu.cn>
2025-07-01 12:08:16 +0000

    Fix the main file error.

--- src/main.py
+++ src/main.py
@@ -1,19 +1,29 @@
 import re
 
+from tokenizer.simple_tokenizer import SimpleTokenizerV2
+
 # 1. prepare the tokens from the traning data
-with open("./resources/the-verdict.txt", "r", encoding="utf-8") as f:
+with open("src/resources/the-verdict.txt", "r", encoding="utf-8") as f:
     raw_text = f.read()
 preprocessed = re.split(r'([,.?_!"()\']|--|\s)', raw_text)
 preprocessed = [item.strip() for item in preprocessed if item.strip()]
 print(len(preprocessed))
 print(preprocessed[:30])
 
-# 8. test the new tokenizer
-# text1 = "Hello, do you like tea?"
-# text2 = "In the sunlit terraces of the palace."
-# text = " <|endoftext|> ".join((text1, text2))
-# print(text)
+# 6. add dedicated tokens
+all_tokens = sorted(list(set(preprocessed)))
+all_tokens.extend(["<|endoftext|>", "<|unk|>"])
+vocab = {token:integer for integer,token in enumerate(all_tokens)}
+print(len(vocab.items()))
+for i, item in enumerate(list(vocab.items())[-5:]):
+    print(item)
 
-# tokenizer = SimpleTokenizerV2(vocab)
-# print(tokenizer.encode(text))
-# print(tokenizer.decode(tokenizer.encode(text)))
+# 8. test the new tokenizer
+text1 = "Hello, do you like tea?"
+text2 = "In the sunlit terraces of the palace."
+text = " <|endoftext|> ".join((text1, text2))
+print(text)
+
+tokenizer = SimpleTokenizerV2(vocab)
+print(tokenizer.encode(text))
+print(tokenizer.decode(tokenizer.encode(text)))
